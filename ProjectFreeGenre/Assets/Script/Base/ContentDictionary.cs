5f79d3da6b8f1d419521eb44487d42f5e86fe4c3
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-15 18:07:20 +0900

    [Add] Add Base scripts
    Add Singleton.cs
    Add ContentDictionary

---
+++ ProjectFreeGenre/Assets/Script/Base/ContentDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDictionaryContent
+{
+    public int ID { get; }
+}
+
+public class ContentDictionary<T> where T : IDictionaryContent
+{
+    public Dictionary<int, T> dict;
+
+    public T GetDict(int key)
+    {
+        return dict[key];
+    }
+
+    public void AddDict(T content)
+    {
+        dict.Add(content.ID, content);
+    }
+
+    public void ClearDict()
+    {
+        dict.Clear();
+    }
+}
---
+++ ProjectFreeGenre/Assets/Script/Base/Singleton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                if(instance == null)
+                {
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this as T;
+
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void DestroySingleton()
+    {
+        Destroy(gameObject);
+    }
+}
