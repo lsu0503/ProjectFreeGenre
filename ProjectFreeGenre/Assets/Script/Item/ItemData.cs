68a517c0579ca8684972d4eb4a26e9d022989bef
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-17 18:17:25 +0900

    [Add] ItemData.cs에 ScriptableObject 파일 생성 메뉴 등록 코드 추가

--- ProjectFreeGenre/Assets/Script/Item/ItemData.cs
+++ ProjectFreeGenre/Assets/Script/Item/ItemData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObject/Item")]
 public class ItemData : ScriptableObject
 {
     public int id;
