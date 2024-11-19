fe551872a463f64c66fd896ec7e419830c8e1e49
whdgur2498 <whdgur2498@gmail.com>
2024-11-19 17:04:04 +0900

    [Add] 플레이어 hp바 UI 추가

--- ProjectFreeGenre/Assets/Script/Player/Player.cs
+++ ProjectFreeGenre/Assets/Script/Player/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     public PlayerMovement playerMove;
     public PlayerEquipment equipment;
+    public PlayerStat playerStat;
 
     private void Awake()
     {
         GameManager.Instance.player = this;
         playerMove = GetComponent<PlayerMovement>();
+        playerStat = GetComponent<PlayerStat>();
     }
 }
---
+++ ProjectFreeGenre/Assets/Script/Player/PlayerStat.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour, IDamage
+{
+    public int MaxHp { get; set; }
+    public int CurrentHp { get; set; }
+    public int Speed { get; set; }
+    public int AttackPower { get; set; }
+    public int AttackDelay { get; set; }
+
+    private void Awake()
+    {
+        MaxHp = 100;
+        CurrentHp = MaxHp;
+    }
+
+    public void Attacked(int damage)
+    {
+        CurrentHp -= damage;
+        if (CurrentHp < 0) CurrentHp = 0;
+        Debug.Log("피격 데미지: " + CurrentHp);
+        //UIManager를 통해 HP UIBar에 현재 체력 업데이트
+    }
+}
