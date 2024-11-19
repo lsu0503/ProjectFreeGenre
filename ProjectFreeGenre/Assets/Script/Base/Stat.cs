42875a2c03feb5fd81c8e8b97cd4edcc82f952e2
whdgur2498 <whdgur2498@gmail.com>
2024-11-19 10:53:08 +0900

    [Add] 넉백, 데미지 인터페이스

---
+++ ProjectFreeGenre/Assets/Script/Base/Stat.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+
+    public int MaxHp { get; set; }
+    public int Speed { get; set; } 
+    public int AttackPower { get; set; } 
+    public int AttackDelay { get; set; } 
+
+}
---
+++ ProjectFreeGenre/Assets/Script/Interface/IKnockback.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IKnockback 
+{
+    void ApplyKnockback(Vector3 direction, float force);
+}
