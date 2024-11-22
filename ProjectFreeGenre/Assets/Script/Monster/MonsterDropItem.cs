933959db3fda100d5976ab709cd1f278cb7370f5
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-22 09:22:10 +0900

    [Fix] 몬스터 아이템 드랍 로직 수정

--- ProjectFreeGenre/Assets/Script/Monster/MonsterDropItem.cs
+++ ProjectFreeGenre/Assets/Script/Monster/MonsterDropItem.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MonsterDropItem : MonoBehaviour
 {
-    public GameObject[] items;
     public MonsterHpSystem hpSystem;
 
     private void Start()
@@ -15,6 +14,8 @@ public class MonsterDropItem : MonoBehaviour
     private void DropItem()
     {
         int randomIndex = Random.Range(0, 3);
-        Instantiate(items[randomIndex], new Vector3(transform.position.x, transform.position.y+0.5f, transform.position.z), Quaternion.identity);
+
+        GameObject dropItem = ItemDataManager.Instance.GetDict_Drop(randomIndex);
+        dropItem.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
     }
 }
